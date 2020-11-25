341c00eedc0cadfc104227481a89599715cedade
GishBird <davidgish@Davids-Mac-mini.local>
2020-11-25 16:16:31 +0700

    add error msg

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
+function validation() {
+    const form = document.getElementById("form");
+    const email = document.getElementById("email").value;
+    const text = document.getElementById("text");
+    const pattern = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
 
-    const form = document.querySelector("form");
-    const button = form.querySelector('[type="submit"]');
-    const msg = 'Please provide a valid email address';
-
-    const error = document.createElement('p');
-    error.classList.add('error_text');
-    error.textContent = msg;
-
-
-    function checkEmail(e) {
-        e.preventDefault();
-        const email = e.target.querySelector('[type="email"]').value;
-        if(!validateEmail(email)) {
-            form.classList.add('error');
-            form.insertBefore(error, button)
-        } else {
-            form.classList.remove('error');
-            form.removeChild(error);
-        }
+    if (email.match(pattern)) 
+    {
+        return true;
         
     }
-    function validateEmail(email) {
-        const re = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-        return re.test(email);
-    }
-
-    form.addEventListener('submit', checkEmail)
-
-
-
-//     var text = document.getElementById("text");
-//     var pattern = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-
-//     if (email.match(pattern)) 
-//     {
-//         return true;
+    else 
+    {
+        text.innerHTML = "Please provide a valid email address";
+        text.style.color = "hsl(354, 100%, 66%)";
+        return false; 
         
-//     }
-//     else 
-//     {
-//         text.innerHTML = "Please provide a valid email address";
-//         text.style.color = "hsl(354, 100%, 66%)";
-//         return false; 
-//     }
-// }
+    }
+}
